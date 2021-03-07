a101d07b0636dc4672c76e61549327cc5bb0ba42
Víctor Martínez <victorcoder2@gmail.com>
2021-03-07 21:42:13 +0100

    mini refactor on main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ mod server;
 use crate::{actors::chat_server::ChatServer, models::AppState, server::init};
 use actix::Actor;
 use actix_web::{App, HttpServer};
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let chat = ChatServer::new().start();
-
     HttpServer::new(move || {
         App::new()
             .data(AppState { chat: chat.clone() })
